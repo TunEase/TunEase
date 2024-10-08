794db646e43c6e25ec2e6ab629fb157bc6f62c69
daligharbi <dgharbi453@gmail.com>
2024-10-08 18:18:41 +0100

    done

---
+++ my-app/components/HomePage/Footer/MainFooter.tsx
@@ -0,0 +1,47 @@
+import { FontAwesome5 } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const Footer: React.FC = () => {
+  return (
+    <View style={styles.footer}>
+      <TouchableOpacity>
+        <FontAwesome5 name="home" size={24} color="#007bff" />
+        <Text style={styles.footerText}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <FontAwesome5 name="heart" size={24} color="#007bff" />
+        <Text style={styles.footerText}>Favorites</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <FontAwesome5 name="user" size={24} color="#007bff" />
+        <Text style={styles.footerText}>Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <FontAwesome5 name="sign-out-alt" size={24} color="#007bff" />
+        <Text style={styles.footerText}>Logout</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+export default Footer;
+const styles = StyleSheet.create({
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "#f0f0f0",
+    borderTopColor: "#ccc",
+    borderTopWidth: 1,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  footerText: {
+    fontSize: 10,
+    color: "#007bff",
+    textAlign: "center",
+  },
+});
