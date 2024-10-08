9a833ebfc64e45571495bc9933a46fde4c3b34db
NciriAbdelmajid <abdelmajidnciri872@gmail.com>
2024-10-08 23:27:37 +0100

    in progress

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,12 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "../my-app/screens/HomePage";
 import CategoryDetails from "./components/HomePage/CategoryDetails";
-
+// import { insertSampleData } from "./services/supabaseClient";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  // useEffect(() => {
+  //   const insertData = async () => {
+  //     try {
+  //       await insertSampleData();
+  //       console.log("Sample data inserted successfully.");
+  //     } catch (error) {
+  //       console.error("Error inserting sample data:", error);
+  //     }
+  //   };
+
+  //   insertData();
+  // }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
