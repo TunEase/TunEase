706a165ca2d930f00da8e798cc7e5360fa174dff
NciriAbdelmajid <abdelmajidnciri872@gmail.com>
2024-10-09 10:13:58 +0100

    sample data

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -3,26 +3,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
 import Home from "../my-app/screens/HomePage";
 import CategoryDetails from "./components/HomePage/CategoryDetails";
-// import { insertSampleData } from "./services/supabaseClient";
+import Categories from "./components/HomePage/Categories";
+import { insertSampleData } from "./services/supabaseClient";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  // useEffect(() => {
-  //   const insertData = async () => {
-  //     try {
-  //       await insertSampleData();
-  //       console.log("Sample data inserted successfully.");
-  //     } catch (error) {
-  //       console.error("Error inserting sample data:", error);
-  //     }
-  //   };
+  useEffect(() => {
+    const insertData = async () => {
+      try {
+        await insertSampleData();
+        console.log("Sample data inserted successfully.");
+      } catch (error) {
+        console.error("Error inserting sample data:", error);
+      }
+    };
 
-  //   insertData();
-  // }, []);
+    insertData();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Categories" component={Categories} />
         <Stack.Screen name="CategoryDetails" component={CategoryDetails} />
       </Stack.Navigator>
     </NavigationContainer>
