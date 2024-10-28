d82e12feeea7ae1f697852c79dc7c3309b851d82
NciriAbdelmajid <abdelmajidnciri872@gmail.com>
2024-10-28 14:02:35 +0100

    appointment details

--- my-app/types/Appointment.tsx
+++ my-app/types/Appointment.tsx
@@ -6,7 +6,8 @@ export type Appointment = {
     service_provider_id: string; // ID of the service provider
     date: string;            // Date of the appointment (e.g., '2024-10-25')
     start_time: string;      // Start time of the appointment (e.g., '10:00:00')
-    end_time: string;        // End time of the appointment (optional)
+    end_time: string;  
+    notes?: string;      // End time of the appointment (optional)
   status: "pending" | "confirmed" | "canceled"; // Status of the appointment
   created_at: string; // Timestamp when the appointment was created
   updated_at?: string; // Timestamp when the appointment was last updated (optional)
